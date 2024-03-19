99ceb167a0e5a94659037aa7157d527ba37ddaf3
Luca Florio <6350968+elleFlorio@users.noreply.github.com>
2024-03-19 22:13:26 +0100

    Add docker-compose down to run.sh script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker-compose build --no-cache && docker-compose up && docker-compose down
+docker-compose down && docker-compose build --no-cache && docker-compose up && docker-compose down
